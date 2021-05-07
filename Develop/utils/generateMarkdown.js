4a161b1727576ad55faaf9de70bb8b74e53a5509
Yuri Kitchin <ykitchin@gmail.com>
2021-05-07 12:23:51 +0000

    Basic readme template with place holder template literals

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,7 +13,44 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  # Title
 
+  # Description
+
+  # Table of Contents
+
+  *[Delployed links](#Deployed-links)
+
+  *[Installation Instructions](#installation Instructions)
+
+  *[Usage](#usage)
+
+  *[Contribution Guidelines](#Contribution Guidelines)
+
+  *[Test Instructions](#Test Instructions)
+
+  *[Liscence](#Liscence)
+
+  # Deployed-links
+
+  #Installation
+  ${answer.installation}
+
+  #Usage
+  ${answer.usage}
+
+  Contribution Guidelines
+  ${answer.contribution}
+
+  Test Instructions
+  ${answer.test}
+
+  Licence
+  ${answer.licence}
+
+  Author
+  ${answer.github}
+  ${answer.email}
 `;
 }
 
